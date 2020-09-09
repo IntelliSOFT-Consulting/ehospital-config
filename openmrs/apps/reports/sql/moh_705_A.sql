7167661349094fc40644ad94a7b22135d884714c
mozzy11 <mozzymutesa@gmail.com>
2020-09-09 13:05:49 +0300

    update script for MOH 705 A

--- openmrs/apps/reports/sql/moh_705_A.sql
+++ openmrs/apps/reports/sql/moh_705_A.sql
@@ -1,247 +1,48 @@
-DROP TABLE IF EXISTS obs_temp;
- CREATE TEMPORARY TABLE obs_temp (
-  id int(11) NOT NULL AUTO_INCREMENT ,
-  diseases varchar(50) DEFAULT NULL,
-  concept_id int(11) DEFAULT NULL,
-  day_1 int(11) DEFAULT 0,
-  day_2 int(11) DEFAULT 0,
-  day_3 int(11) DEFAULT 0 ,
-  day_4 int(11) DEFAULT 0 ,
-  day_5 int(11) DEFAULT 0 ,
-  day_6 int(11) DEFAULT 0 ,
-  day_7 int(11) DEFAULT 0 ,
-  day_8 int(11) DEFAULT 0 ,
-  day_9 int(11) DEFAULT 0 ,
-  day_10 int(11) DEFAULT 0 ,
-  day_11 int(11) DEFAULT 0 ,
-  day_12 int(11) DEFAULT 0 ,
-  day_13 int(11) DEFAULT 0 ,
-  day_14 int(11) DEFAULT 0 ,
-  day_15 int(11) DEFAULT 0 ,
-  day_16 int(11) DEFAULT 0 ,
-  day_17 int(11) DEFAULT 0 ,
-  day_18 int(11) DEFAULT 0 ,
-  day_19 int(11) DEFAULT 0 ,
-  day_20 int(11) DEFAULT 0 ,
-  day_21 int(11) DEFAULT 0 ,
-  day_22 int(11) DEFAULT 0 ,
-  day_23 int(11) DEFAULT 0 ,
-  day_24 int(11) DEFAULT 0 ,
-  day_25 int(11) DEFAULT 0 ,
-  day_26 int(11) DEFAULT 0 ,
-  day_27 int(11) DEFAULT 0 ,
-  day_28 int(11) DEFAULT 0 ,
-  day_29 int(11) DEFAULT 0 ,
-  day_30 int(11) DEFAULT 0 ,
-  day_31 int(11) DEFAULT 0 ,
-  Totals int(11) DEFAULT 0 ,      
-  CONSTRAINT pk_id PRIMARY KEY (id)) ;
-   
-  INSERT INTO obs_temp (concept_id) 
-  SELECT DISTINCT c.concept_id FROM openmrs.concept_name c 
-  WHERE	c.concept_id IN (1,2,3 ,4,5,6 ,1054) ;
-  
-  UPDATE obs_temp ot, openmrs.concept_name c 
-  SET ot.diseases = c.name 
-  WHERE c.concept_id = ot.concept_id
-  AND c.locale = "en" ; 
-  
-  /* this query  requires the first and last day of the same month*/
-  UPDATE obs_temp ot 
-  SET ot.day_1 = (SELECT COUNT(ob.person_id) FROM  openmrs.obs ob ,openmrs.person p  WHERE ob.concept_id = ot.concept_id AND p.person_id = ob.person_id AND  TIMESTAMPDIFF(YEAR, p.birthdate, '#startDate#') < 5 AND ob.obs_datetime = DATE_ADD('#startDate#', INTERVAL 0 DAY))
-  WHERE EXTRACT(DAY FROM '#startDate#') = 1 
-  AND  LAST_DAY('#startDate#') = '#endDate#'
-  AND TIMESTAMPDIFF(MONTH, '#startDate#', '#endDate#') = 0;
-  
-   UPDATE obs_temp ot 
-  SET ot.day_2 = (SELECT COUNT(ob.person_id) FROM  openmrs.obs ob ,openmrs.person p  WHERE ob.concept_id = ot.concept_id AND p.person_id = ob.person_id AND  TIMESTAMPDIFF(YEAR, p.birthdate, '#startDate#') < 5 AND ob.obs_datetime = DATE_ADD('#startDate#', INTERVAL 1 DAY))
-  WHERE EXTRACT(DAY FROM '#startDate#') = 1 
-  AND  LAST_DAY('#startDate#') = '#endDate#'
-  AND TIMESTAMPDIFF(MONTH, '#startDate#', '#endDate#') = 0;
-  
-   UPDATE obs_temp ot 
-  SET ot.day_3 = (SELECT COUNT(ob.person_id) FROM  openmrs.obs ob ,openmrs.person p  WHERE ob.concept_id = ot.concept_id AND p.person_id = ob.person_id AND  TIMESTAMPDIFF(YEAR, p.birthdate, '#startDate#') < 5 AND ob.obs_datetime = DATE_ADD('#startDate#', INTERVAL 2 DAY))
-  WHERE EXTRACT(DAY FROM '#startDate#') = 1 
-  AND  LAST_DAY('#startDate#') = '#endDate#'
-  AND TIMESTAMPDIFF(MONTH, '#startDate#', '#endDate#') = 0;
-  
-   UPDATE obs_temp ot 
-  SET ot.day_4 = (SELECT COUNT(ob.person_id) FROM  openmrs.obs ob ,openmrs.person p  WHERE ob.concept_id = ot.concept_id AND p.person_id = ob.person_id AND  TIMESTAMPDIFF(YEAR, p.birthdate, '#startDate#') < 5 AND ob.obs_datetime = DATE_ADD('#startDate#', INTERVAL 3 DAY))
-  WHERE EXTRACT(DAY FROM '#startDate#') = 1 
-  AND  LAST_DAY('#startDate#') = '#endDate#'
-  AND TIMESTAMPDIFF(MONTH, '#startDate#', '#endDate#') = 0;
-  
-   UPDATE obs_temp ot 
-  SET ot.day_5 = (SELECT COUNT(ob.person_id) FROM  openmrs.obs ob ,openmrs.person p  WHERE ob.concept_id = ot.concept_id AND p.person_id = ob.person_id AND  TIMESTAMPDIFF(YEAR, p.birthdate, '#startDate#') < 5 AND ob.obs_datetime = DATE_ADD('#startDate#', INTERVAL 4 DAY))
-  WHERE EXTRACT(DAY FROM '#startDate#') = 1 
-  AND  LAST_DAY('#startDate#') = '#endDate#'
-  AND TIMESTAMPDIFF(MONTH, '#startDate#', '#endDate#') = 0;
-  
-   UPDATE obs_temp ot 
-  SET ot.day_6 = (SELECT COUNT(ob.person_id) FROM  openmrs.obs ob ,openmrs.person p  WHERE ob.concept_id = ot.concept_id AND p.person_id = ob.person_id AND  TIMESTAMPDIFF(YEAR, p.birthdate, '#startDate#') < 5 AND ob.obs_datetime = DATE_ADD('#startDate#', INTERVAL 5 DAY))
-  WHERE EXTRACT(DAY FROM '#startDate#') = 1 
-  AND  LAST_DAY('#startDate#') = '#endDate#'
-  AND TIMESTAMPDIFF(MONTH, '#startDate#', '#endDate#') = 0;
-  
-   UPDATE obs_temp ot 
-  SET ot.day_7 = (SELECT COUNT(ob.person_id) FROM  openmrs.obs ob ,openmrs.person p  WHERE ob.concept_id = ot.concept_id AND p.person_id = ob.person_id AND  TIMESTAMPDIFF(YEAR, p.birthdate, '#startDate#') < 5 AND ob.obs_datetime = DATE_ADD('#startDate#', INTERVAL 6 DAY))
-  WHERE EXTRACT(DAY FROM '#startDate#') = 1 
-  AND  LAST_DAY('#startDate#') = '#endDate#'
-  AND TIMESTAMPDIFF(MONTH, '#startDate#', '#endDate#') = 0;
-  
-   UPDATE obs_temp ot 
-  SET ot.day_8 = (SELECT COUNT(ob.person_id) FROM  openmrs.obs ob ,openmrs.person p  WHERE ob.concept_id = ot.concept_id AND p.person_id = ob.person_id AND  TIMESTAMPDIFF(YEAR, p.birthdate, '#startDate#') < 5 AND ob.obs_datetime = DATE_ADD('#startDate#', INTERVAL 7 DAY))
-  WHERE EXTRACT(DAY FROM '#startDate#') = 1 
-  AND  LAST_DAY('#startDate#') = '#endDate#'
-  AND TIMESTAMPDIFF(MONTH, '#startDate#', '#endDate#') = 0;
-  
-   UPDATE obs_temp ot 
-  SET ot.day_9 = (SELECT COUNT(ob.person_id) FROM  openmrs.obs ob ,openmrs.person p  WHERE ob.concept_id = ot.concept_id AND p.person_id = ob.person_id AND  TIMESTAMPDIFF(YEAR, p.birthdate, '#startDate#') < 5 AND ob.obs_datetime = DATE_ADD('#startDate#', INTERVAL 8 DAY))
-  WHERE EXTRACT(DAY FROM '#startDate#') = 1 
-  AND  LAST_DAY('#startDate#') = '#endDate#'
-  AND TIMESTAMPDIFF(MONTH, '#startDate#', '#endDate#') = 0;
-  
-   UPDATE obs_temp ot 
-  SET ot.day_10 = (SELECT COUNT(ob.person_id) FROM  openmrs.obs ob ,openmrs.person p  WHERE ob.concept_id = ot.concept_id AND p.person_id = ob.person_id AND  TIMESTAMPDIFF(YEAR, p.birthdate, '#startDate#') < 5 AND ob.obs_datetime = DATE_ADD('#startDate#', INTERVAL 9 DAY))
-  WHERE EXTRACT(DAY FROM '#startDate#') = 1 
-  AND  LAST_DAY('#startDate#') = '#endDate#'
-  AND TIMESTAMPDIFF(MONTH, '#startDate#', '#endDate#') = 0;
-  
-   UPDATE obs_temp ot 
-  SET ot.day_11 = (SELECT COUNT(ob.person_id) FROM  openmrs.obs ob ,openmrs.person p  WHERE ob.concept_id = ot.concept_id AND p.person_id = ob.person_id AND  TIMESTAMPDIFF(YEAR, p.birthdate, '#startDate#') < 5 AND ob.obs_datetime = DATE_ADD('#startDate#', INTERVAL 10 DAY))
-  WHERE EXTRACT(DAY FROM '#startDate#') = 1 
-  AND  LAST_DAY('#startDate#') = '#endDate#'
-  AND TIMESTAMPDIFF(MONTH, '#startDate#', '#endDate#') = 0;
-  
-   UPDATE obs_temp ot 
-  SET ot.day_12 = (SELECT COUNT(ob.person_id) FROM  openmrs.obs ob ,openmrs.person p  WHERE ob.concept_id = ot.concept_id AND p.person_id = ob.person_id AND  TIMESTAMPDIFF(YEAR, p.birthdate, '#startDate#') < 5 AND ob.obs_datetime = DATE_ADD('#startDate#', INTERVAL 11 DAY))
-  WHERE EXTRACT(DAY FROM '#startDate#') = 1 
-  AND  LAST_DAY('#startDate#') = '#endDate#'
-  AND TIMESTAMPDIFF(MONTH, '#startDate#', '#endDate#') = 0;
-  
-   UPDATE obs_temp ot 
-  SET ot.day_13 = (SELECT COUNT(ob.person_id) FROM  openmrs.obs ob ,openmrs.person p  WHERE ob.concept_id = ot.concept_id AND p.person_id = ob.person_id AND  TIMESTAMPDIFF(YEAR, p.birthdate, '#startDate#') < 5 AND ob.obs_datetime = DATE_ADD('#startDate#', INTERVAL 12 DAY))
-  WHERE EXTRACT(DAY FROM '#startDate#') = 1 
-  AND  LAST_DAY('#startDate#') = '#endDate#'
-  AND TIMESTAMPDIFF(MONTH, '#startDate#', '#endDate#') = 0;
-  
-   UPDATE obs_temp ot 
-  SET ot.day_14 = (SELECT COUNT(ob.person_id) FROM  openmrs.obs ob ,openmrs.person p  WHERE ob.concept_id = ot.concept_id AND p.person_id = ob.person_id AND  TIMESTAMPDIFF(YEAR, p.birthdate, '#startDate#') < 5 AND ob.obs_datetime = DATE_ADD('#startDate#', INTERVAL 13 DAY))
-  WHERE EXTRACT(DAY FROM '#startDate#') = 1 
-  AND  LAST_DAY('#startDate#') = '#endDate#'
-  AND TIMESTAMPDIFF(MONTH, '#startDate#', '#endDate#') = 0;
-  
-   UPDATE obs_temp ot 
-  SET ot.day_15 = (SELECT COUNT(ob.person_id) FROM  openmrs.obs ob ,openmrs.person p  WHERE ob.concept_id = ot.concept_id AND p.person_id = ob.person_id AND  TIMESTAMPDIFF(YEAR, p.birthdate, '#startDate#') < 5 AND ob.obs_datetime = DATE_ADD('#startDate#', INTERVAL 14 DAY))
-  WHERE EXTRACT(DAY FROM '#startDate#') = 1 
-  AND  LAST_DAY('#startDate#') = '#endDate#'
-  AND TIMESTAMPDIFF(MONTH, '#startDate#', '#endDate#') = 0;
-  
-   UPDATE obs_temp ot 
-  SET ot.day_16 = (SELECT COUNT(ob.person_id) FROM  openmrs.obs ob ,openmrs.person p  WHERE ob.concept_id = ot.concept_id AND p.person_id = ob.person_id AND  TIMESTAMPDIFF(YEAR, p.birthdate, '#startDate#') < 5 AND ob.obs_datetime = DATE_ADD('#startDate#', INTERVAL 15 DAY))
-  WHERE EXTRACT(DAY FROM '#startDate#') = 1 
-  AND  LAST_DAY('#startDate#') = '#endDate#'
-  AND TIMESTAMPDIFF(MONTH, '#startDate#', '#endDate#') = 0;
-  
-   UPDATE obs_temp ot 
-  SET ot.day_17 = (SELECT COUNT(ob.person_id) FROM  openmrs.obs ob ,openmrs.person p  WHERE ob.concept_id = ot.concept_id AND p.person_id = ob.person_id AND  TIMESTAMPDIFF(YEAR, p.birthdate, '#startDate#') < 5 AND ob.obs_datetime = DATE_ADD('#startDate#', INTERVAL 16 DAY))
-  WHERE EXTRACT(DAY FROM '#startDate#') = 1 
-  AND  LAST_DAY('#startDate#') = '#endDate#'
-  AND TIMESTAMPDIFF(MONTH, '#startDate#', '#endDate#') = 0;
-  
-   UPDATE obs_temp ot 
-  SET ot.day_18 = (SELECT COUNT(ob.person_id) FROM  openmrs.obs ob ,openmrs.person p  WHERE ob.concept_id = ot.concept_id AND p.person_id = ob.person_id AND  TIMESTAMPDIFF(YEAR, p.birthdate, '#startDate#') < 5 AND ob.obs_datetime = DATE_ADD('#startDate#', INTERVAL 17 DAY))
-  WHERE EXTRACT(DAY FROM '#startDate#') = 1 
-  AND  LAST_DAY('#startDate#') = '#endDate#'
-  AND TIMESTAMPDIFF(MONTH, '#startDate#', '#endDate#') = 0;
-  
-   UPDATE obs_temp ot 
-  SET ot.day_19 = (SELECT COUNT(ob.person_id) FROM  openmrs.obs ob ,openmrs.person p  WHERE ob.concept_id = ot.concept_id AND p.person_id = ob.person_id AND  TIMESTAMPDIFF(YEAR, p.birthdate, '#startDate#') < 5 AND ob.obs_datetime = DATE_ADD('#startDate#', INTERVAL 18 DAY))
-  WHERE EXTRACT(DAY FROM '#startDate#') = 1 
-  AND  LAST_DAY('#startDate#') = '#endDate#'
-  AND TIMESTAMPDIFF(MONTH, '#startDate#', '#endDate#') = 0;
-  
-   UPDATE obs_temp ot 
-  SET ot.day_20 = (SELECT COUNT(ob.person_id) FROM  openmrs.obs ob ,openmrs.person p  WHERE ob.concept_id = ot.concept_id AND p.person_id = ob.person_id AND  TIMESTAMPDIFF(YEAR, p.birthdate, '#startDate#') < 5 AND ob.obs_datetime = DATE_ADD('#startDate#', INTERVAL 19 DAY))
-  WHERE EXTRACT(DAY FROM '#startDate#') = 1 
-  AND  LAST_DAY('#startDate#') = '#endDate#'
-  AND TIMESTAMPDIFF(MONTH, '#startDate#', '#endDate#') = 0;
-  
-   UPDATE obs_temp ot 
-  SET ot.day_21 = (SELECT COUNT(ob.person_id) FROM  openmrs.obs ob ,openmrs.person p  WHERE ob.concept_id = ot.concept_id AND p.person_id = ob.person_id AND  TIMESTAMPDIFF(YEAR, p.birthdate, '#startDate#') < 5 AND ob.obs_datetime = DATE_ADD('#startDate#', INTERVAL 20 DAY))
-  WHERE EXTRACT(DAY FROM '#startDate#') = 1 
-  AND  LAST_DAY('#startDate#') = '#endDate#'
-  AND TIMESTAMPDIFF(MONTH, '#startDate#', '#endDate#') = 0;
-  
-   UPDATE obs_temp ot 
-  SET ot.day_22 = (SELECT COUNT(ob.person_id) FROM  openmrs.obs ob ,openmrs.person p  WHERE ob.concept_id = ot.concept_id AND p.person_id = ob.person_id AND  TIMESTAMPDIFF(YEAR, p.birthdate, '#startDate#') < 5 AND ob.obs_datetime = DATE_ADD('#startDate#', INTERVAL 21 DAY))
-  WHERE EXTRACT(DAY FROM '#startDate#') = 1 
-  AND  LAST_DAY('#startDate#') = '#endDate#'
-  AND TIMESTAMPDIFF(MONTH, '#startDate#', '#endDate#') = 0;
-  
-   UPDATE obs_temp ot 
-  SET ot.day_23 = (SELECT COUNT(ob.person_id) FROM  openmrs.obs ob ,openmrs.person p  WHERE ob.concept_id = ot.concept_id AND p.person_id = ob.person_id AND  TIMESTAMPDIFF(YEAR, p.birthdate, '#startDate#') < 5 AND ob.obs_datetime = DATE_ADD('#startDate#', INTERVAL 22 DAY))
-  WHERE EXTRACT(DAY FROM '#startDate#') = 1 
-  AND  LAST_DAY('#startDate#') = '#endDate#'
-  AND TIMESTAMPDIFF(MONTH, '#startDate#', '#endDate#') = 0;
-  
-   UPDATE obs_temp ot 
-  SET ot.day_24 = (SELECT COUNT(ob.person_id) FROM  openmrs.obs ob ,openmrs.person p  WHERE ob.concept_id = ot.concept_id AND p.person_id = ob.person_id AND  TIMESTAMPDIFF(YEAR, p.birthdate, '#startDate#') < 5 AND ob.obs_datetime = DATE_ADD('#startDate#', INTERVAL 23 DAY))
-  WHERE EXTRACT(DAY FROM '#startDate#') = 1 
-  AND  LAST_DAY('#startDate#') = '#endDate#'
-  AND TIMESTAMPDIFF(MONTH, '#startDate#', '#endDate#') = 0;
-  
-   UPDATE obs_temp ot 
-  SET ot.day_25 = (SELECT COUNT(ob.person_id) FROM  openmrs.obs ob ,openmrs.person p  WHERE ob.concept_id = ot.concept_id AND p.person_id = ob.person_id AND  TIMESTAMPDIFF(YEAR, p.birthdate, '#startDate#') < 5 AND ob.obs_datetime = DATE_ADD('#startDate#', INTERVAL 24 DAY))
-  WHERE EXTRACT(DAY FROM '#startDate#') = 1 
-  AND  LAST_DAY('#startDate#') = '#endDate#'
-  AND TIMESTAMPDIFF(MONTH, '#startDate#', '#endDate#') = 0;
-  
-   UPDATE obs_temp ot 
-  SET ot.day_26 = (SELECT COUNT(ob.person_id) FROM  openmrs.obs ob ,openmrs.person p  WHERE ob.concept_id = ot.concept_id AND p.person_id = ob.person_id AND  TIMESTAMPDIFF(YEAR, p.birthdate, '#startDate#') < 5 AND ob.obs_datetime = DATE_ADD('#startDate#', INTERVAL 25 DAY))
-  WHERE EXTRACT(DAY FROM '#startDate#') = 1 
-  AND  LAST_DAY('#startDate#') = '#endDate#'
-  AND TIMESTAMPDIFF(MONTH, '#startDate#', '#endDate#') = 0;
-  
-   UPDATE obs_temp ot 
-  SET ot.day_27 = (SELECT COUNT(ob.person_id) FROM  openmrs.obs ob ,openmrs.person p  WHERE ob.concept_id = ot.concept_id AND p.person_id = ob.person_id AND  TIMESTAMPDIFF(YEAR, p.birthdate, '#startDate#') < 5 AND ob.obs_datetime = DATE_ADD('#startDate#', INTERVAL 26 DAY))
-  WHERE EXTRACT(DAY FROM '#startDate#') = 1 
-  AND  LAST_DAY('#startDate#') = '#endDate#'
-  AND TIMESTAMPDIFF(MONTH, '#startDate#', '#endDate#') = 0;
-  
-   UPDATE obs_temp ot 
-  SET ot.day_28 = (SELECT COUNT(ob.person_id) FROM  openmrs.obs ob ,openmrs.person p  WHERE ob.concept_id = ot.concept_id AND p.person_id = ob.person_id AND  TIMESTAMPDIFF(YEAR, p.birthdate, '#startDate#') < 5 AND ob.obs_datetime = DATE_ADD('#startDate#', INTERVAL 27 DAY))
-  WHERE EXTRACT(DAY FROM '#startDate#') = 1 
-  AND  LAST_DAY('#startDate#') = '#endDate#'
-  AND TIMESTAMPDIFF(MONTH, '#startDate#', '#endDate#') = 0;
-  
-   UPDATE obs_temp ot 
-  SET ot.day_29 = (SELECT COUNT(ob.person_id) FROM  openmrs.obs ob ,openmrs.person p  WHERE ob.concept_id = ot.concept_id AND p.person_id = ob.person_id AND  TIMESTAMPDIFF(YEAR, p.birthdate, '#startDate#') < 5 AND ob.obs_datetime = DATE_ADD('#startDate#', INTERVAL 28 DAY))
-  WHERE EXTRACT(DAY FROM '#startDate#') = 1 
-  AND  LAST_DAY('#startDate#') = '#endDate#'
-  AND TIMESTAMPDIFF(MONTH, '#startDate#', '#endDate#') = 0;
-  
-   UPDATE obs_temp ot 
-  SET ot.day_30 = (SELECT COUNT(ob.person_id) FROM  openmrs.obs ob ,openmrs.person p  WHERE ob.concept_id = ot.concept_id AND p.person_id = ob.person_id AND  TIMESTAMPDIFF(YEAR, p.birthdate, '#startDate#') < 5 AND ob.obs_datetime = DATE_ADD('#startDate#', INTERVAL 29 DAY))
-  WHERE EXTRACT(DAY FROM '#startDate#') = 1 
-  AND  LAST_DAY('#startDate#') = '#endDate#'
-  AND TIMESTAMPDIFF(MONTH, '#startDate#', '#endDate#') = 0;
-  
-  UPDATE obs_temp ot 
-  SET ot.day_31 = (SELECT COUNT(ob.person_id) FROM  openmrs.obs ob ,openmrs.person p  WHERE ob.concept_id = ot.concept_id AND p.person_id = ob.person_id AND  TIMESTAMPDIFF(YEAR, p.birthdate, '#startDate#') < 5 AND ob.obs_datetime = DATE_ADD('#startDate#', INTERVAL 30 DAY))
-  WHERE EXTRACT(DAY FROM '#startDate#') = 1 
-  AND  LAST_DAY('#startDate#') = '#endDate#'
-  AND TIMESTAMPDIFF(MONTH, '#startDate#', '#endDate#') = 0;
-
-  UPDATE obs_temp ot 
-  SET ot.Totals  = (SELECT day_1 + day_2 + day_3 + day_4 + day_5 + day_6 + day_7 + day_8 + day_9 + day_10 + day_11 + day_12 + day_13 + day_14 + day_15 + day_16
-  + day_17 + day_18 + day_19 + day_20 + day_21 + day_22 + day_23 + day_24 + day_25 + day_26 + day_27 + day_28 + day_29 + day_30 + day_31 ) 
-  WHERE ot.concept_id IS NOT NULL;
-    
- DROP TABLE IF EXISTS view_temp;
- CREATE TEMPORARY TABLE view_temp
- SELECT * FROM obs_temp;
-
- ALTER TABLE view_temp DROP COLUMN concept_id;
-
- SELECT * FROM view_temp ; 
+SELECT
+	c.name AS Diagnosis,
+	SUM(CASE DAY(encounter_datetime) WHEN 1 THEN 1 ELSE 0 END) AS 1st,
+	SUM(CASE DAY(encounter_datetime) WHEN 2 THEN 1 ELSE 0 END) AS 2nd,
+	SUM(CASE DAY(encounter_datetime) WHEN 3 THEN 1 ELSE 0 END) AS 3rd,
+	SUM(CASE DAY(encounter_datetime) WHEN 4 THEN 1 ELSE 0 END) AS 4th,
+	SUM(CASE DAY(encounter_datetime) WHEN 5 THEN 1 ELSE 0 END) AS 5th,
+	SUM(CASE DAY(encounter_datetime) WHEN 6 THEN 1 ELSE 0 END) AS 6th,
+	SUM(CASE DAY(encounter_datetime) WHEN 7 THEN 1 ELSE 0 END) AS 7th,
+	SUM(CASE DAY(encounter_datetime) WHEN 8 THEN 1 ELSE 0 END) AS 8th,
+	SUM(CASE DAY(encounter_datetime) WHEN 9 THEN 1 ELSE 0 END) AS 9th,
+	SUM(CASE DAY(encounter_datetime) WHEN 10 THEN 1 ELSE 0 END) AS 10th,
+	SUM(CASE DAY(encounter_datetime) WHEN 11 THEN 1 ELSE 0 END) AS 11th,
+	SUM(CASE DAY(encounter_datetime) WHEN 12 THEN 1 ELSE 0 END) AS 12th,
+	SUM(CASE DAY(encounter_datetime) WHEN 13 THEN 1 ELSE 0 END) AS 13th,
+	SUM(CASE DAY(encounter_datetime) WHEN 14 THEN 1 ELSE 0 END) AS 14th,
+	SUM(CASE DAY(encounter_datetime) WHEN 15 THEN 1 ELSE 0 END) AS 15th,
+	SUM(CASE DAY(encounter_datetime) WHEN 16 THEN 1 ELSE 0 END) AS 16th,
+	SUM(CASE DAY(encounter_datetime) WHEN 17 THEN 1 ELSE 0 END) AS 17th,
+	SUM(CASE DAY(encounter_datetime) WHEN 18 THEN 1 ELSE 0 END) AS 18th,
+	SUM(CASE DAY(encounter_datetime) WHEN 19 THEN 1 ELSE 0 END) AS 19th,
+	SUM(CASE DAY(encounter_datetime) WHEN 20 THEN 1 ELSE 0 END) AS 20th,
+	SUM(CASE DAY(encounter_datetime) WHEN 21 THEN 1 ELSE 0 END) AS 21st,
+	SUM(CASE DAY(encounter_datetime) WHEN 22 THEN 1 ELSE 0 END) AS 22nd,
+	SUM(CASE DAY(encounter_datetime) WHEN 23 THEN 1 ELSE 0 END) AS 23rd,
+	SUM(CASE DAY(encounter_datetime) WHEN 24 THEN 1 ELSE 0 END) AS 24th,
+	SUM(CASE DAY(encounter_datetime) WHEN 25 THEN 1 ELSE 0 END) AS 25th,
+	SUM(CASE DAY(encounter_datetime) WHEN 26 THEN 1 ELSE 0 END) AS 26th,
+	SUM(CASE DAY(encounter_datetime) WHEN 27 THEN 1 ELSE 0 END) AS 27th,
+	SUM(CASE DAY(encounter_datetime) WHEN 28 THEN 1 ELSE 0 END) AS 28th,
+	SUM(CASE DAY(encounter_datetime) WHEN 29 THEN 1 ELSE 0 END) AS 29th,
+	SUM(CASE DAY(encounter_datetime) WHEN 30 THEN 1 ELSE 0 END) AS 30th,
+	SUM(CASE DAY(encounter_datetime) WHEN 31 THEN 1 ELSE 0 END) AS 31th,
+	SUM(CASE WHEN DAY(encounter_datetime)IS NOT NULL THEN 1 ELSE 0 END) AS Totals
+FROM openmrs.obs o 
+INNER JOIN openmrs.encounter e ON o.encounter_id = e.encounter_id
+INNER JOIN openmrs.concept_name c ON c.concept_id = o.value_coded AND locale = 'en' AND c.locale_preferred = 1
+INNER JOIN openmrs.person p ON p.person_id = o.person_id 
+WHERE 
+   encounter_datetime BETWEEN '#startDate#' AND '#endDate#'
+   AND value_coded IS NOT NULL
+   /*add all the concept ids for diagnoses here*/
+   AND o.concept_id IN (1, 1) 
+   AND TIMESTAMPDIFF(YEAR, p.birthdate, '#startDate#') < 5
+   AND EXTRACT(DAY FROM '#startDate#') = 1 
+   AND LAST_DAY('#startDate#') = '#endDate#'
+   AND TIMESTAMPDIFF(MONTH, '#startDate#', '#endDate#') = 0
+GROUP BY c.name ;
